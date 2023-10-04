0655439ffee7e0a28194dd6b616df2d31c295e73
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2023-10-04 20:00:11 -0300

    Clean code

--- src/components/P5App/Index.tsx
+++ src/components/P5App/Index.tsx
@@ -1,23 +1,7 @@
 import { ReactP5Wrapper } from "react-p5-wrapper";
 import { game } from "./Game";
-import { useState, useEffect } from "react"
 
 export default function P5App() {
-
-    const [idade, setIdade] = useState<number | null>(null);
-    const timer_ms = 300;
-
-    useEffect(() => {
-      const interval = setInterval(() => {
-        const idadeFromLocalStorage = localStorage.getItem('idade');
-        if (idadeFromLocalStorage) {
-          setIdade(parseInt(idadeFromLocalStorage, 10));
-        }
-      }, timer_ms);
-  
-      // Limpar o intervalo quando o componente for desmontado para evitar vazamento de memória
-      return () => clearInterval(interval);
-    }, []); // O array vazio como segundo argumento faz com que este efeito só seja executado uma vez após a montagem do componente
 
     return (
         <>
